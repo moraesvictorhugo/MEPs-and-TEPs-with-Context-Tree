19ce4a5417fb1d8a597233168eb8601b8ae5ab69
Victor Hugo Moraes <119534834+moraesvictorhugo@users.noreply.github.com>
2026-05-11 18:25:21 -0300

    .

--- main_preprocessing.py
+++ main_preprocessing.py
@@ -53,6 +53,7 @@ Salvar com scipy.io.savemat('V01.mat', {'data': ...})
 Exemplo em: /home/victomoraes/Documents/GitHub/EEG_Retrieving/statistical_analysis/EEGretrieving_pre/preprocessed_data/V01.mat
 
 - EEG epochs (15 ms to 415 ms) -> adapted to cut TMS artifact at 250 Hz of sampling rate
+- Editar arquivo TERNARY_CONDITION.m to choice electrodes to analyze
 '''
 
 
